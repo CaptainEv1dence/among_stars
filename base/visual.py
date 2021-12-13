696c7ebef4337a98e9e6d77b04f23ee05c62cd11
mariia-kiko <kovalenko.m@phystech.edu>
2021-12-13 04:12:35 +0300

    cucumber

--- base/visual.py
+++ base/visual.py
@@ -93,11 +93,11 @@ class Drawer:
 class DrawableObject(pg.sprite.Sprite):
     def __init__(self, obj):
         self.obj = obj
-        pg.sprite.Sprite.__init__(self)
-        self.image = starship_img = pg.image.load(os.path.join(r'C:\Users\petrk\among_stars\img', 'rock.png')).convert()
-        self.image.set_colorkey((0, 0, 0))
-        self.rect = self.image.get_rect()
-        self.rect.center = (300, 300)
+        #pg.sprite.Sprite.__init__(self)
+        #self.image = starship_img = pg.image.load(os.path.join(r'C:\Users\petrk\among_stars\img', 'rock.png')).convert()
+        #self.image.set_colorkey((0, 0, 0))
+        #self.rect = self.image.get_rect()
+        #self.rect.center = (300, 300)
 
     def draw(self, surface):
         '''рисует круглый объект на заданной поверхности, 
